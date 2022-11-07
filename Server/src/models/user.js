ab19b52bae827fdc4fe9d683fdfd6c5606461b2b
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-07 17:12:00 +0500

    Git Init

--- Server/src/models/user.js
+++ Server/src/models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -26,3 +29,27 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.methods.generateAuthToken = function () {
+
+    const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, {
+        expiresIn: "7d",
+    });
+    return token;
+
+};
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+
+    const schema = Joi.object({
+        firstName: Joi.string().required().label("First Name"),
+        lastName: Joi.string().required().label("Last Name"),
+        email: Joi.string().email().required().label("Email"),
+        password: passwordComplexity().required().label("Password"),
+    });
+    return schema.validate(data);
+
+};
+
+module.exports = { User, validate };
