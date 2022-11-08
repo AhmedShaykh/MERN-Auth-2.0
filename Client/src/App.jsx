dc415ac04b8046c401bcdcdc7d8d8960a456b652
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-08 16:11:55 +0500

    Git Init

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,11 @@
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./Components/Main";
+import Signup from "./Components/Singup";
+import Login from "./Components/Login";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Routes></Routes>
   );
 }
 
