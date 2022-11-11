4d8e623d06844730a228b906947cd7daf35f4e9e
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-11 18:28:29 +0500

    Git Init

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,8 +4,16 @@ import Signup from "./Components/Singup";
 import Login from "./Components/Login";
 
 function App() {
+
+  const user = localStorage.getItem("token");
+
   return (
-    <Routes></Routes>
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate replace to="/login" />} />
+    </Routes>
   );
 }
 
